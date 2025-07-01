683d1f85edd961467a8b7c368f6e06eca28c5e3f
BrindaBudhabhatti <brinda.budhabhatti@gmail.com>
2025-07-01 13:20:08 +0530

    change -5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -74,6 +74,7 @@ export default function Home() {
 
   return (
     <div className="space-y-5">
+      <div className="flex justify-center mt-4 md:mt-30">
       <section className="text-center py-2">
         <div className="mb-6 flex justify-center text-primary">
           {/* Updated Cyber Crime Logo */}
